a54cc77b615dadf61fddd89feab2737a43743853
Russell Gayle <russ.gayle@gmail.com>
2014-02-18 23:17:02 -0700

    Get a GLFW up

--- lesson3/common.cpp
+++ lesson3/common.cpp
@@ -5,20 +5,20 @@ void queso::init(int argc, char* argv[]) {
   google::SetVersionString("0.0.3");
   google::ParseCommandLineFlags(&argc, &argv, true);
 
-  // Init google logging
+  // Google logging preferences
   FLAGS_log_link = ".";
   FLAGS_alsologtostderr = true;
   FLAGS_colorlogtostderr = true;
   google::InitGoogleLogging(argv[0]);
 
-  // GLFW
-  LOG(INFO) << "Starting GLFW... ";
+  // GLFW state
   glfwSetErrorCallback(queso::error_callback);
 
+  LOG(INFO) << "Starting GLFW.. ";
   if (!glfwInit()) {
-    std::cerr << "Fatal." << std::endl;
     LOG(FATAL) << "Fatal: Could not start GLFW3";
   }
+  LOG(INFO) << "... done!";
 
   // Mac
   glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
@@ -28,6 +28,18 @@ void queso::init(int argc, char* argv[]) {
 
   // Preferences
   glfwWindowHint(GLFW_SAMPLES, 4); // anti-aliasing
+
+  //// GLEW
+  //LOG(INFO) << "Starting GLEW.. ";
+  //glewExperimental = GL_TRUE;
+  //glewInit();
+  //LOG(INFO) << "... done!";
+
+  //// Print some basic info
+  //const GLubyte* renderer = glGetString(GL_RENDERER);
+  //const GLubyte* version = glGetString(GL_VERSION);
+  //LOG(INFO) << "Renderer: " << renderer;
+  //LOG(INFO) << "OpenGL version supported: " << version;
 }
 
 void queso::error_callback(int error, const char* description) {
--- lesson3/main.cpp
+++ lesson3/main.cpp
@@ -1,8 +1,35 @@
 #include "common.h"
 
+int width = 640;
+int height = 480; 
+
+void window_callback(GLFWwindow* window, int p_width, int p_height) {
+  width = p_width;
+  height = p_height;
+}
+
 int main(int argc, char* argv[])
 {
   queso::init(argc, argv);
 
-  LOG(INFO) << "Logged";
+  // Set up a window
+  GLFWwindow* window = glfwCreateWindow(width, height, "Gooey cheese. Lesson 3", NULL, NULL);
+  if (!window) {
+    glfwTerminate();
+    LOG(FATAL) << "Could not open a window with GLFW3";
+  }
+  glfwSetWindowSizeCallback(window, window_callback);
+  glfwMakeContextCurrent(window);
+
+  // Yay, window is up. 
+  glewExperimental = GL_TRUE;
+  glewInit();
+
+  // Query system
+  const GLubyte* renderer = glGetString(GL_RENDERER);
+  const GLubyte* version = glGetString(GL_VERSION);
+  LOG(INFO) << "Renderer: " << renderer;
+  LOG(INFO) << "OpenGL version supported: " << version;
+
+  LOG(INFO) << argv[0] << " completed. Thanks for playing.";
 }
