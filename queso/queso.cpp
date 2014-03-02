8a986221c0dd3f0f488db44628476f8da9e865c3
Russell Gayle <russ.gayle@gmail.com>
2014-03-02 13:41:48 -0700

    Back on Mac. Fix cmake for lesson5 (and, queso too)

--- queso/queso.cpp
+++ queso/queso.cpp
@@ -25,10 +25,10 @@ GLFWwindow* queso::init(int argc, char* argv[], const std::string& appName) {
   LOG(INFO) << "... done!";
 
   // Mac
-  //glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
-  //glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 2);
-  //glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
-  //glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+  glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+  glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 2);
+  glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
+  glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 
   // Preferences
   glfwWindowHint(GLFW_SAMPLES, 4); // anti-aliasing
